782e2e8f6c6fb3305ab1d88beb6d5ec15661c97f
john <john.cranney+git@gmail.com>
2016-12-31 11:53:25 +1100

    Added readme

---
+++ README.sh
@@ -0,0 +1,6 @@
+This is a bunch of bad python that controls my pool pump via an arduino hacked onto a consumer remote controlled mains plug.
+
+It has three main functions:
+1) It polls the local weather station to get the current wind speed, and turns on when the wind is high (so that leaves landing in the pool get skimmed out)
+2) It runs a few hours a day on a schedule
+3) It turns on when request via a web interface- I have this hooked up to android and tasker so I get voice control
